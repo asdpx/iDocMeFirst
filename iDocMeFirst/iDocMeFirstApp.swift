83988f91cf99efa6f5b0b38eaff55e963f79e3eb
Vladimir Grishchenkov <gvadev0106@gmail.com>
2026-01-02 10:27:12 +0300

    Initial Commit

---
+++ iDocMeFirst/iDocMeFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDocMeFirstApp.swift
+//  iDocMeFirst
+//
+//  Created by Vladimir Grishchenkov on 02.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct iDocMeFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
